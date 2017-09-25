ea0c5dcffc5113b9e57db28d5612198942cecf23
bestegriffin <griffinbeste@gmail.com>
2017-09-25 16:23:46 -0400

    cant get spotify to work

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'z1nkmDigOUh181jNneafEXZ7s',
+  consumer_secret: '3Kju3LvsnekHwB8DQYev60jnnE2flTMkjJRekHkOklu6CmDK63',
+  access_token_key: '4581726488-IGP47Vk5yzKTyP1Druh5V935sradl1p77MILgZj',
+  access_token_secret: 'ym2EkAtm6mBtM74O2kwGewVSNIaLvJXLyhhVuVRheUheC',
+}
+
+module.exports = twitterKeys;
+
+
+
